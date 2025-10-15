3a6619dc560a2ee130af2a00354953a94e3a6bbf
Georgina Kennedy <georginak@aderica01.erica.med.unsw.edu.au@ip-hex-c0a88489.aderica01.erica.med.unsw.edu.au>
2025-10-15 01:13:38 +0000

    for ecog demo

--- _version.py
+++ _version.py
@@ -1,5 +1,6 @@
 # version history:
 # cava lang alpha = 0.1.0
 # upgraded alpha to work with python 3.10 = 0.2.0
+# upgraded alpha to work with poetry & python 3.11 = 0.3.0
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
